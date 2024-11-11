14f8f80c7fdbb1d54b2e9bb5267c25467fc2ef9e
Jeonghoon Park <hoondb@naver.com>
2024-11-12 02:57:07 +0900

    refactor: 위치 정보 관련 코드 삭제

--- app/src/main/java/com/kakaotech/team25M/ui/ManagerApplication.kt
+++ app/src/main/java/com/kakaotech/team25M/ui/ManagerApplication.kt
@@ -5,14 +5,12 @@ import androidx.appcompat.app.AppCompatDelegate
 import com.amazonaws.mobileconnectors.s3.transferutility.TransferNetworkLossHandler
 import com.kakaotech.team25M.BuildConfig
 import com.kakao.sdk.common.KakaoSdk
-import com.kakao.vectormap.KakaoMapSdk
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class ManagerApplication: Application() {
     override fun onCreate() {
         super.onCreate()
-        initializeKakaoMapSdk()
         initializeKakaoSdk()
         initializeTransferNetworkLossHandler()
         setLightMode()
@@ -24,10 +22,6 @@ class ManagerApplication: Application() {
 
     private fun initializeTransferNetworkLossHandler() {
         TransferNetworkLossHandler.getInstance(this)
-    }
-
-    private fun initializeKakaoMapSdk() {
-        KakaoMapSdk.init(this, BuildConfig.KAKAO_API_KEY)
     }
 
     private fun initializeKakaoSdk() {
